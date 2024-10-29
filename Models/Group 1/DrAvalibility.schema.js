9a76a724b01f806f26d7b756028ac407cf07a87f
Abdul Rehman <145707047+ProCodingTech@users.noreply.github.com>
2024-10-30 00:46:04 +0500

    Update DrAvalibility.schema.js

--- Models/Group 1/DrAvalibility.schema.js
+++ Models/Group 1/DrAvalibility.schema.js
@@ -1,46 +1,31 @@
-const mongoose = require("mongoose");
+const mongoose = require('mongoose');
 
 // Doctor Availability Schema
-const availabilitySchema = new mongoose.Schema(
-  {
-    doctorId: {
-      type: String,
-      required: true,
-      match: /^[0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12}$/, // UUID format
+const availabilitySchema = new mongoose.Schema({
+    doctorId: { 
+      type: String, 
+      required: true, 
+      match: /^[0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12}$/ // UUID format
     },
-    days: [
-      {
-        day: {
-          type: String,
-          enum: [
-            "Monday",
-            "Tuesday",
-            "Wednesday",
-            "Thursday",
-            "Friday",
-            "Saturday",
-            "Sunday",
-          ],
-          required: true,
-        },
-        timeStart: {
-          type: String,
-          required: true, // Format: HH:mm
-        },
-        timeEnd: {
-          type: String,
-          required: true, // Format: HH:mm
-        },
+    days: [{
+      day: { 
+        type: String, 
+        enum: ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"], 
+        required: true 
       },
-    ],
-  },
-  {
+      timeStart: { 
+        type: String, 
+        required: true // Format: HH:mm
+      },
+      timeEnd: { 
+        type: String, 
+        required: true // Format: HH:mm
+      }
+    }]
+  }, {
     timestamps: true,
-  }
-);
+  });
 
-const DoctorAvailability = mongoose.model(
-  "DoctorAvailability",
-  availabilitySchema
-);
+
+const DoctorAvailability = mongoose.model('DoctorAvailability', availabilitySchema);
 module.exports = DoctorAvailability;
