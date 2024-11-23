eab49f089a8be930cdb28fef9be4284070700af2
Bilal Akbar <geo.bilal7@gmail.com>
2024-11-23 20:27:55 +0500

    Refactor Analytics and Medicine schemas, add Supplier schema

--- Models/Group 12/Analytics.schema.js
+++ Models/Group 12/Analytics.schema.js
@@ -1,39 +1,63 @@
-const mongoose = require('mongoose');
+const { ref } = require("joi");
+const mongoose = require("mongoose");
 
-const analyticsServiceSchema = new mongoose.Schema({
-    medicationUsageReport: [{
-      medicationId: {
-        type: String,
-        required: true,
-        match: /^[0-9a-fA-F]{8}-[0-9a-fA-F]{4}-[0-9a-fA-F]{4}-[0-9a-fA-F]{4}-[0-9a-fA-F]{12}$/, // UUID format
-      },
-      totalQuantityUsed: {
-        type: Number,
-        required: true,
-        min: 0,
-      },
-      reportDate: {
-        type: Date,
-        required: true,
-      },
-    }],
-    prescriptionTrends: [{
-      medicationId: {
-        type: String,
-        required: true,
-        match: /^[0-9a-fA-F]{8}-[0-9a-fA-F]{4}-[0-9a-fA-F]{4}-[0-9a-fA-F]{4}-[0-9a-fA-F]{12}$/, // UUID format
-      },
-      totalPrescriptions: {
-        type: Number,
-        required: true,
-        min: 0,
-      },
-      trendAnalysisDate: {
-        type: Date,
-        required: true,
-      },
-    }],
+const AnalyticsSchema = new mongoose.Schema({
+	pharmacyId: {
+		type: String,
+		required: true,
+		ref: "Pharmacy",
+	},
+	reportMonth: {
+		type: Number,
+		required: true,
+		min: 1,
+		max: 12,
+	},
+	reportYear: {
+		type: Number,
+		required: true,
+		min: 2000,
+	},
+	reportDate: {
+		type: Date,
+		required: true,
+	},
+	medicationUsageReport: [
+		{
+			medicationId: {
+				type: String,
+				ref: "Medicine",
+				required: true,
+				match: /^[0-9a-fA-F]{8}-[0-9a-fA-F]{4}-[0-9a-fA-F]{4}-[0-9a-fA-F]{4}-[0-9a-fA-F]{12}$/, // UUID format
+			},
+			totalSold: {
+				type: Number,
+				required: true,
+				min: 0,
+			},
+		},
+	],
+	prescriptionTrends: [
+		{
+			medicationId: {
+				type: String,
+				required: true,
+				ref: "Medicine",
+				match: /^[0-9a-fA-F]{8}-[0-9a-fA-F]{4}-[0-9a-fA-F]{4}-[0-9a-fA-F]{4}-[0-9a-fA-F]{12}$/, // UUID format
+			},
+			totalPrescriptions: {
+				type: Number,
+				required: true,
+				min: 0,
+			},
+		},
+	],
+	totalPrescriptionsProcessed: {
+		type: Number,
+		required: true,
+		min: 0,
+	},
 });
 
-const AnalyticsService = mongoose.model('AnalyticsService', analyticsServiceSchema);
-module.exports = AnalyticsService;
+const model = mongoose.model("Analytics", AnalyticsSchema);
+module.exports = model;
